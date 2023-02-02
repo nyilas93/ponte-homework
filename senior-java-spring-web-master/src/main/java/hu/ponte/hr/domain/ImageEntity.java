2426faf02bf5cf0275be57b2e6bb3023d0e191ee
Nyilas Mihály <nyilas.misi93@gmail.com>
2023-02-02 15:31:02 +0100

    create entity and repository for saving images

---
+++ senior-java-spring-web-master/src/main/java/hu/ponte/hr/domain/ImageEntity.java
@@ -0,0 +1,32 @@
+package hu.ponte.hr.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
+@Data
+@NoArgsConstructor
+public class ImageEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String name;
+    private String type;
+    @Lob
+    private byte[] data;
+    private String sign;
+
+    public ImageEntity(String name, String type, byte[] data, String sign) {
+        this.name = name;
+        this.type = type;
+        this.data = data;
+        this.sign = sign;
+    }
+}
